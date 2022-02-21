af0c2a7bea689889ddfe6517c840ea8e7b4bb401
Kevin Glowacz <kevin@glowacz.info>
2022-02-21 17:26:26 -0600

    Split out vendor chunk

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,21 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: "production",
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
   devServer: {
     client: {
       overlay: {
